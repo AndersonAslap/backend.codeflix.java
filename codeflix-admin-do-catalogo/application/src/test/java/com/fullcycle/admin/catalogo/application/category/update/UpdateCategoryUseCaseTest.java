dcacbe8d2c5d747b92f7009ccad523444113761f
Anderson Paiva <anderson.works1998@gmail.com>
2025-08-28 21:50:12 -0300

    test: criação do teste de caso de uso de atualização de categoria, testado o fluxo de atualizar categoria

---
+++ codeflix-admin-do-catalogo/application/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -0,0 +1,74 @@
+package com.fullcycle.admin.catalogo.application.category.update;
+
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultUpdateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    // 1. Teste do caminho feliz
+    // 2. Teste passando uma propiedade inválida (name)
+    // 3. Teste atualizando uma categoria para inativa
+    // 4. Teste simulando um erro genérico vindo do gateway
+    // 5. Teste atualizar categoria passando ID inválido
+
+    @Test
+    public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() {
+        final var category = Category.newCategory("Film", null, true);
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedId = category.getId();
+
+        final var command = UpdateCategoryCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        when(categoryGateway.findById(eq(expectedId)))
+                .thenReturn(Optional.of(category));
+
+        when(categoryGateway.update(any()))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        final var output = useCase.execute(command).get();
+
+        Assertions.assertNotNull(output);
+        Assertions.assertNotNull(output.id());
+
+        Mockito.verify(categoryGateway, times(1)).findById(eq(expectedId));
+
+        Mockito.verify(categoryGateway, times(1)).update(argThat(
+                updateCategory ->
+                        Objects.equals(expectedName, updateCategory.getName())
+                            && Objects.equals(expectedDescription, updateCategory.getDescription())
+                            && Objects.equals(expectedIsActive, updateCategory.isActive())
+                            && Objects.equals(expectedId, updateCategory.getId())
+                            && Objects.equals(category.getCreatedAt(), updateCategory.getCreatedAt())
+                            && category.getUpdatedAt().isBefore(updateCategory.getUpdatedAt())
+                            && Objects.isNull(updateCategory.getDeletedAt())
+        ));
+    }
+}
